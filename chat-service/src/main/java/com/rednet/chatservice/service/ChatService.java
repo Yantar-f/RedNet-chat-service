034ec95ada2054ba735203cf5997268f15b3da52
yantar <shevchenko9542@gmail.com>
2023-12-13 22:02:16 +0600

    interface code style fixed

--- chat-service/src/main/java/com/rednet/chatservice/service/ChatService.java
+++ chat-service/src/main/java/com/rednet/chatservice/service/ChatService.java
@@ -9,19 +9,19 @@ import com.rednet.chatservice.entity.key.MessageKey;
 import java.util.List;
 
 public interface ChatService {
-    List<ConversationPreload>   getConversationsPreload(String userID);
-    Conversation                createConversation(String creatorID, List<String> conversationMembersIDs, String title);
+    List<ConversationPreload>   getConversationsPreload (String userID);
+    Conversation                createConversation      (String creatorID, List<String> conversationMembersIDs, String title);
 
-    List<Message>   getLastMessages(ConversationKey conversationKey, int count, String userID);
-    List<Message>   getMessagesFrom(MessageKey messageKey, int count, String userID);
-    Message         pushMessage(ConversationKey conversationKey, String text, String userID);
-    Message         editMessage(MessageKey messageKey, String text, String userID);
-    void            deleteMessages(List<MessageKey> messageKeys, String userID);
+    List<Message>   getLastMessages (ConversationKey conversationKey, int count, String userID);
+    List<Message>   getMessagesFrom (MessageKey messageKey, int count, String userID);
+    Message         pushMessage     (ConversationKey conversationKey, String text, String userID);
+    Message         editMessage     (MessageKey messageKey, String text, String userID);
+    void            deleteMessages  (List<MessageKey> messageKeys, String userID);
 
-    void disableConversationNotifications(ConversationKey conversationKey, String userID);
-    void enableConversationNotifications(ConversationKey conversationKey, String userID);
+    void disableConversationNotifications   (ConversationKey conversationKey, String userID);
+    void enableConversationNotifications    (ConversationKey conversationKey, String userID);
 
-    List<String>    getConversationMembersIDs(ConversationKey conversationKey);
-    void            addConversationMembers(ConversationKey conversationKey, List<String> userIDs, String executorID);
-    void            removeConversationMembers(ConversationKey conversationKey, List<String> userIDs, String executorID);
+    List<String>    getConversationMembersIDs   (ConversationKey conversationKey);
+    void            addConversationMembers      (ConversationKey conversationKey, List<String> userIDs, String executorID);
+    void            removeConversationMembers   (ConversationKey conversationKey, List<String> userIDs, String executorID);
 }
